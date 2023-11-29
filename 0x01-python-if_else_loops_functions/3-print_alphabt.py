e8cecdd9afd8c8c6c61eedbf9efc9a23c7698f65
MmeliNtshakala <mmeliroot@gmail.com>
2023-11-29 13:14:04 -0800

    alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-i = 0
-while i < 26: 
-    if i != 4 and i != 16:   
-        print('{:c}'. format(i + 97), end='')
-    i = i + 1
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
