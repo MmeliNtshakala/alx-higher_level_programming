56b056ee8833918480bd8e0d578a6cc9f2abdf48
MmeliNtshakala <mmeliroot@gmail.com>
2023-12-06 09:57:38 -0800

    mest

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    best_score = 0
-    best_score1 = None
+    best_v = 0
+    best_k = None
     if a_dictionary:
-        for x, y in a_dictionary.items():
-            if x > best_score:
-                best_score = x
-                best_score1 = y
-        return best_score1
+        for k, v in a_dictionary.items():
+            if v > best_v:
+                best_v = v
+                best_k = k
+        return best_k
     else:
         return None
