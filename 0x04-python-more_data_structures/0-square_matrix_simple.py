dd62b431eceb5f85568e8424cdc4d8de34795aa9
MmeliNtshakala <mmeliroot@gmail.com>
2023-12-06 05:00:21 -0800

    simple matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def suare_marix_simple(matrix=[]):
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+More data structures focusing on dictionaries and lambda
