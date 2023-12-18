3d3a90fbd87a402335cc8f88e576954234e15bb6
MmeliNtshakala <mmeliroot@gmail.com>
2023-12-18 05:45:22 -0800

    list integgers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list[], x=0):
+    ctr = 0
+
+    for y in range(0, x):
+        try:
+            print("{:d}".format(my_list[y],end="")
+            ctr += 1
+        except (TypeError, ValueError):
+            pass
+        print()
+        return ctr
