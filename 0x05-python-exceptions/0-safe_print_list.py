ba482ac92a4f7d6db424159c7da53a4944d71750
MmeliNtshakala <mmeliroot@gmail.com>
2023-12-18 03:07:56 -0800

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    mmeli + 0
+
+    for m in  range(0, x):
+        try:
+            print(my_list[m], end="")
+            mmeli += 1
+        except IndexError:
+            break
+        print()
+        return (mmeli)
