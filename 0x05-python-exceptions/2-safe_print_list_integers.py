95d29d192608d46dbbf753ac0fd1b9fcd9eaa7f4
MmeliNtshakala <mmeliroot@gmail.com>
2023-12-18 05:52:23 -0800

    list integgers1

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,9 @@
 
 def safe_print_list_integers(my_list[], x=0):
     ctr = 0
-    for index in range(0, x):
+    for y in range(0, x):
         try:
-            print("{:d}".format(my_list[ndex], end="")
+            print("{:d}".format(my_list[y]), end="")
             ctr += 1
         except (TypeError, ValueError):
             pass
