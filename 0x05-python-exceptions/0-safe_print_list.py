9da90888c48e81018ac402582bf1e0f63b47e65c
MmeliNtshakala <mmeliroot@gmail.com>
2023-12-18 03:29:57 -0800

    fix task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 
     for m in  range(0, x):
         try:
-            print(my_list[m], end="")
+            print(f"{my_list[m]}", end="")
             mmeli += 1
         except IndexError:
             break
