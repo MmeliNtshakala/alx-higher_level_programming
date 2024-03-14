f9991380e141d61f447f6e52a3f91bf8aecd89cb
MmeliNtshakala <mmeliroot@gmail.com>
2024-03-13 22:55:19 -0700

    java amazing

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,12 +1,3 @@
 #!/usr/bin/node
 
-const args = process.argv;
-if (args.length === 2) {
-  console.log('No argument');
-}
-if (args.length === 3) {
-  console.log('Argument found');
-}
-if (args.length > 3) {
-  console.log('Arguments found');
-}
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
